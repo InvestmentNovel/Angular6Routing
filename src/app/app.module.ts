7f62ef11585dcb4509fec62dfa8489b00425c53f
KrunalLathiya <krunal@appdividend.com>
2018-05-14 10:44:48 +0530

    Complete Angular 6 Routing Example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
 
 import { AppComponent } from './app.component';
+import { NewsComponent } from './components/news/news.component';
+import { MarketComponent } from './components/market/market.component';
+import { ToolsComponent } from './components/tools/tools.component';
+
+const routes: Routes = [
+  {
+    path: 'news',
+    component: NewsComponent
+  },
+  {
+    path: 'market',
+    component: MarketComponent
+  },
+  {
+    path: 'tools',
+    component: ToolsComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsComponent,
+    MarketComponent,
+    ToolsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    SlimLoadingBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
